ebe214ef569d8cc69e8a90771b7288eeafa2d53b
Deniz Mert Kaya <wi24b027@technikum-wien.at>
2025-06-17 10:19:35 +0200

    adding tests

---
+++ TicTacToe/Tests/BoardTest.java
@@ -0,0 +1,62 @@
+import org.example.Board;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+public class BoardTest {
+    @Test
+    public void testIfBoardIsEmpty(){
+        //Arrange
+        Board board = new Board();
+        board.place(2, 2, 'X');
+        char[][] cells = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+        //Act
+        board.clear();
+
+        //Assert
+        assertTrue(Arrays.deepEquals(cells, board.getCells()));
+    }
+
+    @Test
+    public void testIfBoardIsNotEmpty(){
+        //Arrange
+        Board board = new Board();
+        board.place(2, 2, 'X');
+        char[][] cells = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+        //Act
+        board.clear();
+        board.place(1, 2, 'X');
+
+        //Assert
+        assertTrue(!Arrays.deepEquals(cells, board.getCells()));
+    }
+
+    @Test
+    public void testIfMarkerIsPlaced(){
+        //Arrange
+        Board board = new Board();
+        char[][] cells = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', 'X'}};
+
+        //Act
+        board.place(2, 2, 'X');
+
+        // Assert
+        assertTrue(Arrays.deepEquals(cells, board.getCells()));
+    }
+
+    @Test
+    public void testIfMarkerIsMisplaced(){
+        //Arrange
+        Board board = new Board();
+        char[][] cells = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', 'X'}};
+
+        //Act
+        board.place(2, 1, 'X');
+
+        // Assert
+        assertTrue(!Arrays.deepEquals(cells, board.getCells()));
+    }
+
+}
--- TicTacToe/src/main/java/org/example/Board.java
+++ TicTacToe/src/main/java/org/example/Board.java
@@ -8,6 +8,10 @@ public class Board {
         clear();
     }
 
+    public char[][] getCells() {
+        return cells;
+    }
+
     public void clear() {
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
